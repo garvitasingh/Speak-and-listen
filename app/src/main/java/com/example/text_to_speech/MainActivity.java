b2404e47efe441b6a7eb86ca680d0392e9055440
Garvita singh <garvitasingh936@gmail.com>
2022-07-31 15:54:33 +0530

    initial commit

---
+++ app/src/main/java/com/example/text_to_speech/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.text_to_speech;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.Voice;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView Text;
+    Button btnText;
+    TextToSpeech textToSpeech;
+
+    private ImageView Mic;
+    private TextView Speech_to_text;
+    private TextView remark;
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1;
+    private int a=4;
+    private int intValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Text = findViewById(R.id.Text);
+        btnText = findViewById(R.id.btnText);
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                // if No error is found then only it will run
+                if(i!=TextToSpeech.ERROR){
+                    textToSpeech.setLanguage(Locale.US);
+                }
+            }
+        });
+
+        btnText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak(Text.getText().toString(),TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+
+        // Speech to text
+        Mic = findViewById(R.id.mic);
+        Speech_to_text = findViewById(R.id.speech_to_text);
+        remark=findViewById(R.id.remark);
+
+        Mic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent
+                        = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                        Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak the answer aloud");
+
+                try {
+                    startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+                }
+                catch (Exception e) {
+                    Toast.makeText(MainActivity.this, " " + e.getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(
+                        RecognizerIntent.EXTRA_RESULTS);
+                Speech_to_text.setText(
+                        Objects.requireNonNull(result).get(0));
+
+                try {
+                    intValue = Integer.parseInt(Speech_to_text.getText().toString());
+                } catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this, "Only numeric answer is allowed",
+                                    Toast.LENGTH_LONG).show();
+                }
+                if(intValue==a){
+                    remark.setText("Correct answer");
+                }else{
+                    remark.setText("Incorrect answer, try again");
+                }
+                textToSpeech.speak(remark.getText().toString(),TextToSpeech.QUEUE_FLUSH,null);
+            }
+        }
+
+    }
+}
